37e6c35993d347fb544e7b6682f2907b12e13f60
pw598 <106397483+pw598@users.noreply.github.com>
2023-03-11 12:21:10 -0700

    Add files via upload

---
+++ NLTK-Examples/langmodel/med_lang_model.py
@@ -0,0 +1,84 @@
+from __future__ import division
+
+import math
+import os.path
+
+import cPickle
+import glob
+import nltk
+from nltk.corpus.reader import XMLCorpusReader
+
+class LangModel:
+  def __init__(self, order, alpha, sentences):
+    self.order = order
+    self.alpha = alpha
+    if order > 1:
+      self.backoff = LangModel(order - 1, alpha, sentences)
+      self.lexicon = None
+    else:
+      self.backoff = None
+      self.n = 0
+    self.ngramFD = nltk.FreqDist()
+    lexicon = set()
+    for sentence in sentences:
+      words = nltk.word_tokenize(sentence)
+      wordNGrams = nltk.ngrams(words, order)
+      for wordNGram in wordNGrams:
+        self.ngramFD.inc(wordNGram)
+        if order == 1:
+          lexicon.add(wordNGram)
+          self.n += 1
+    self.v = len(lexicon)
+
+  def logprob(self, ngram):
+    return math.log(self.prob(ngram))
+  
+  def prob(self, ngram):
+    if self.backoff != None:
+      freq = self.ngramFD[ngram]
+      backoffFreq = self.backoff.ngramFD[ngram[1:]]
+      if freq == 0:
+        return self.alpha * self.backoff.prob(ngram[1:])
+      else:
+        return freq / backoffFreq
+    else:
+      # laplace smoothing to handle unknown unigrams
+      return ((self.ngramFD[ngram] + 1) / (self.n + self.v))
+
+def train():
+  if os.path.isfile("lm.bin"):
+    return
+  files = glob.glob("data/*.xml")
+  sentences = []
+  i = 0
+  for file in files:
+    if i > 0 and i % 500 == 0:
+      print("%d/%d files loaded, #-sentences: %d" %
+        (i, len(files), len(sentences)))
+    dir, file = file.split("/")
+    reader = XMLCorpusReader(dir, file)
+    sentences.extend(nltk.sent_tokenize(" ".join(reader.words())))
+    i += 1
+  lm = LangModel(3, 0.4, sentences)
+  cPickle.dump(lm, open("lm.bin", "wb"))
+
+def test():
+  lm1 = cPickle.load(open("lm.bin", 'rb'))
+  testFile = open("sentences.test", 'rb')
+  for line in testFile:
+    sentence = line.strip()
+    print "SENTENCE:", sentence,
+    words = nltk.word_tokenize(sentence)
+    wordTrigrams = nltk.trigrams(words)
+    slogprob = 0
+    for wordTrigram in wordTrigrams:
+      logprob = lm1.logprob(wordTrigram)
+      slogprob += logprob
+    print "(", slogprob / len(words), ")"
+
+def main():
+  train()
+  test()
+
+if __name__ == "__main__":
+  main()
